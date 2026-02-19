daf14eb97aafe90382ca8e61b3af429f7158d12f
AlbaGG95 <albaganduxe95@gmail.com>
2026-02-19 11:27:37 +0100

    test: setup vitest and rtl

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,18 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  test: {
+    environment: "jsdom",
+    globals: true,
+    setupFiles: "./src/tests/setupTests.js",
+    css: false,
+    coverage: {
+      provider: "v8",
+      reporter: ["text", "html"],
+      lines: 75,
+      functions: 75,
+      branches: 75,
+      statements: 75,
+    },
+  },
 })
